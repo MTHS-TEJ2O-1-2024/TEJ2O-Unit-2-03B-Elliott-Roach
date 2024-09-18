47a85cba4aeace476b06908beeed4b12e26e8403
Elliott-Roach <elliott.roach@mths.ca>
2024-09-18 15:11:02 +0000

    changed sleep time

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,13 +7,13 @@ This module is a Micro:bit MicroPython program calculats area and primiter
 from microbit import *
 
 display.clear()
-sleep(1)
+sleep(1000)
 
 
 display.scroll("Area =" + str(5 * 3))
 
-sleep(1)
+sleep(1000)
 display.clear()
-sleep(1)
+sleep(1000)
 
 display.scroll("Primiter =" + str(2 * (2 + 3)))
