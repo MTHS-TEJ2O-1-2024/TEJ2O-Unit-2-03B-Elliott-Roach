8cc357e0eaf1b624a36a8c610ef46c70d4136fcd
Elliott-Roach <elliott.roach@mths.ca>
2024-09-18 15:03:43 +0000

    made code do math

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,13 +7,13 @@ This module is a Micro:bit MicroPython program calculats area and primiter
 from microbit import *
 
 display.clear()
-sleep(1000)
+sleep(1)
 
 
-display.scroll("Area =" + ( 5 * 3 ))
+display.scroll("Area =" + str(5 * 3))
 
-sleep(1000)
+sleep(1)
 display.clear()
-sleep(1000)
+sleep(1)
 
-display.scroll("Primiter =" + ( 2 * (2 + 3)))
+display.scroll("Primiter =" + str(2 * (2 + 3)))
